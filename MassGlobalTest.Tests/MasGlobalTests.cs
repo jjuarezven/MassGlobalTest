e4b1bbe3c6782c0a95c3ec407f6128a52275b866
JJuarez <jose.juarez@yuxipacific.com>
2019-02-14 18:14:51 -0500

    Adding test case

---
+++ MassGlobalTest.Tests/MasGlobalTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace MassGlobalTest.BLL.Tests
+{
+	[TestClass()]
+	public class MasGlobalTests
+	{
+		[TestMethod()]
+		public void ListsAreEqual()
+		{
+			var rules = new Rules();			
+			var listFromRules = rules.GetEmployees().ToList();
+			Assert.AreEqual(2, listFromRules.Count);
+		}
+	}
+}
