3f7f6eacafa19c9b49621e621cecbca1a123488f
JJuarez <jose.juarez@yuxipacific.com>
2019-02-14 11:57:18 -0500

    adding web api controller and renaming project names

---
+++ MassGlobalTest/Controllers/EmployeeController.cs
@@ -0,0 +1,22 @@
+﻿using MassGlobalTest.BLL;
+using System.Linq;
+using System.Web.Http;
+
+namespace MassGlobalTest.Controllers
+{
+	public class EmployeeController : ApiController
+	{
+		// GET api/<controller>
+		public IHttpActionResult Get(int? employeeId = 0)
+		{
+			var rules = new Rules();
+			var employees = rules.GetEmployees(employeeId.Value);
+			if (!employees.Any())
+			{
+				return NotFound();
+
+			}
+			return Ok(employees);
+		}
+	}
+}
--- MassGlobalTest/Controllers/HomeController.cs
+++ MassGlobalTest/Controllers/HomeController.cs
@@ -1,27 +1,56 @@
-﻿using MassGlobalTest.BLL;
-using MassGlobalTest.Models;
+﻿using MassGlobalTest.Models;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 
 namespace MassGlobalTest.Controllers
 {
 	public class HomeController : Controller
-    {
-        // GET: Home
-        public ActionResult Index()
-        {	
+	{
+		// GET: Home
+		public ActionResult Index()
+		{
 			return View();
 		}
-		
+
 		public ActionResult GetEmployees(int? employeeId)
-		{				
-			if (employeeId.HasValue)
+		{
+			IEnumerable<Employee> employeesList = null;
+			using (var client = new HttpClient())
 			{
-				var rules = new Rules();
-				var employees = rules.GetEmployees(employeeId.Value);
-				return View(employees);
+				client.BaseAddress = new Uri("http://localhost:7557/api/");
+
+
+				Task<HttpResponseMessage> responseTask = null;
+				if (!employeeId.HasValue)
+				{
+					responseTask = client.GetAsync("Employee");
+				}
+				else
+				{
+					responseTask = client.GetAsync("Employee/" + "?employeeId=" + employeeId.ToString());
+				}
+				responseTask.Wait();
+
+				var result = responseTask.Result;
+				if (result.IsSuccessStatusCode)
+				{
+					var readTask = result.Content.ReadAsAsync<IList<Employee>>();
+					readTask.Wait();
+
+					employeesList = readTask.Result;
+				}
+				else
+					employeesList = Enumerable.Empty<Employee>();
+
+				ModelState.AddModelError(string.Empty, "This Employee is not registered, please try with a valid Employee Id");
 			}
-			return View();
+			return View(employeesList);
 		}
+
 	}
 }
+
